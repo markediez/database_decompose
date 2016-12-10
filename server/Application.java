bae4b05ae1baa1a82c1f8a555c0bf6026552465f
Mark Diez <markediez@gmail.com>
2016-12-09 23:50:03 -0800

    Implement relation and start stub for functional dependencies

--- server/Application.java
+++ server/Application.java
@@ -5,23 +5,86 @@
  */
 
  import java.util.Scanner;
+ import java.util.ArrayList;
 
  public class Application {
    public static void main(String[] args) {
      // Initialize variables
      Scanner input = new Scanner(System.in);
+     ArrayList<String> relation = new ArrayList<>();
 
      // Prompt for a valid relation
-     String relation;
+     String sRelation;
      do {
-       relation = "";
-       System.out.print("Enter relation R separated by a whitespace >>");
-       relation = input.nextLine();
+       sRelation = "";
 
-     } while (!isValidRelation(relation));
+       System.out.print("Enter relation R separated by a whitespace >>");
+       sRelation = input.nextLine();
+     } while (!isValidRelation(sRelation));
+
+     // Store and output relation
+     System.out.println("Relation saved.")
+     relation = createRelation(sRelation);
+     showRelation(relation);
+
+     // Prompt for valid set of functional dependencies
+     String functionalDependency;
+     do {
+       showCurrentFD();
+       functionalDependency = "";
+
+       System.out.println("Enter a single functional dependency 'A B -> C D' >>");
+       functionalDependency = input.nextLine();
+
+       // Exit loop on -1 flag
+       if (functionalDependency.equalsIgnoreCase("-1")) {
+         break;
+       }
+
+     } while (isValidFunctionalDependency())
    }
 
-   public static boolean isValidRelation(String relation) {  
+   public static void showRelation(ArrayList<String> relation) {
+     System.out.print("R(");
+     for (String attribute : relation) {
+       System.out.print(attribute + ",");
+     }
+     System.out.print(")");
+   }
+
+   public static void showCurrentFD() {
+
+   }
+
+
+   public ArrayList<String> createRelation(String relationString) {
+     return null;
+   }
+
+   /**
+    * Return true if the string is a valid relation
+    * Current valid format: "some_col another_col A neXt"
+    * Separate function in case a new format is desired
+    * @param relation - Attributes of the relation separated by whitespace
+    * @return true / false
+    */
+   public static boolean isValidFunctionalDependency(String functionalDependency) {
+     boolean valid = false;
+     // Separate by ->
+     // if size != 2 then the format is wrong
+
+     // [0] = LHS , [1] = RHS, for both sides, ensure that the
+     return valid;
+   }
+
+   /**
+    * Return true if the string is a valid relation
+    * Current valid format: "some_col another_col A neXt"
+    * Separate function in case a new format is desired
+    * @param relation - Attributes of the relation separated by whitespace
+    * @return true / false
+    */
+   public static boolean isValidRelation(String relation) {
      return relation.length() > 0;
    }
  }
