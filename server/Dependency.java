dca32fe399f50c3f5f2fa4a6a3604bafada32e30
Mark Diez <markediez@gmail.com>
2016-12-15 10:05:27 -0800

    Add dependency class for fds

---
+++ server/Dependency.java
@@ -0,0 +1,24 @@
+public class Dependency {
+  private Set mLHS, mRHS;
+
+  public Dependency() {
+    mLHS = new Set();
+    mRHS = new Set();
+  }
+
+  public Dependency(Set LHS, Set RHS) {
+    mLHS = LHS;
+    mRHS = RHS;
+  }
+
+  public void setLHS(Set LHS) {
+    mLHS = LHS;
+  }
+
+  public void setRHS(Set RHS) {
+    mRHS = RHS;
+  }
+
+  public Set getLHS() { return mLHS; }
+  public Set getRHS() { return mRHS; }
+}
