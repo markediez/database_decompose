179c658520b9cedc8fa58475272ff9e06095c2d0
Mark Diez <markediez@gmail.com>
2016-12-15 19:05:11 -0800

    Implement toString for relation

--- server/Relation.java
+++ server/Relation.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+
 public class Relation {
-  
+  private Set mAttrs;
+  private ArrayList<Dependency> mFunctionalDependencies;
+
+  public Relation(Set attrs, ArrayList<Dependency> functionalDependencies) {
+    mAttrs = new Set(attrs);
+    mFunctionalDependencies = new ArrayList<>();
+    mFunctionalDependencies.addAll(functionalDependencies);
+  }
+
+  public void setAttrs(Set attrs) {
+    mAttrs = new Set(attrs);
+  }
+
+  public void setFunctionalDependencies(ArrayList<Dependency> functionalDependencies) {
+    mFunctionalDependencies = new ArrayList<>();
+    mFunctionalDependencies.addAll(functionalDependencies);
+  }
+
+  @Override
+  public String toString() {
+    String ret = "R(" + mAttrs.toString() + ")\n";
+    for (Dependency d : mFunctionalDependencies) {
+      ret += d.toString() + "\n";
+    }
+    return ret;
+  }
 }
